d51fa57891ce6060041870865c0567b4e117367d
AlexeySolomatin <74787973+AlexeySolomatin@users.noreply.github.com>
2026-07-17 14:19:52 +0300

    Update uninstall.sh

--- modules/disk/uninstall.sh
+++ modules/disk/uninstall.sh
@@ -1,1 +1,19 @@
+#!/usr/bin/env bash
+#
+# -----------------------------------------------------------------------------
+# Lite Server Monitor (LSM)
+# Disk Module Uninstaller
+# -----------------------------------------------------------------------------
 
+set -Eeuo pipefail
+
+services_stop_and_disable lsm-disk.timer
+
+deploy_remove_file /etc/systemd/system/lsm-disk.service
+deploy_remove_file /etc/systemd/system/lsm-disk.timer
+
+deploy_remove_directory /opt/lsm/modules/disk
+
+services_daemon_reload
+
+log_success "Disk module removed."
