9640b2ea23e6bfa9b331a661a108dd3ca32b3453
AlexeySolomatin <74787973+AlexeySolomatin@users.noreply.github.com>
2026-07-17 14:01:32 +0300

    Create 08_finish.sh

---
+++ installer/steps/08_finish.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+step_finish() {
+
+    log_step "Installation completed"
+
+    echo
+    log_success "Lite Server Monitor has been installed successfully."
+    echo
+
+    echo "Run:"
+    echo
+    echo "    lsm status"
+    echo
+
+}
