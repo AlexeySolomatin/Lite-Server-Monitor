fc6c0229f0345b5e351c24826abd0836330b9a2a
AlexeySolomatin <74787973+AlexeySolomatin@users.noreply.github.com>
2026-07-17 14:06:41 +0300

    Update ups.sh

--- installer/screens/ups.sh
+++ installer/screens/ups.sh
@@ -1,1 +1,52 @@
+#!/usr/bin/env bash
+#
+# -----------------------------------------------------------------------------
+# Lite Server Monitor (LSM)
+# UPS Configuration Screen
+# -----------------------------------------------------------------------------
 
+INSTALL_UPS=false
+UPS_PROFILE=""
+
+screen_ups() {
+
+    wizard_header
+
+    if ! wizard_yes_no "Install APC UPS support?"; then
+        return
+    fi
+
+    INSTALL_UPS=true
+
+    echo
+    echo "Configuration"
+    echo
+    echo "1) APC Default"
+    echo "2) Configure later"
+    echo
+
+    while true; do
+
+        read -rp "Select [1-2]: " answer
+
+        case "${answer}" in
+
+            1)
+
+                UPS_PROFILE="default"
+
+                break
+                ;;
+
+            2)
+
+                UPS_PROFILE="later"
+
+                break
+                ;;
+
+        esac
+
+    done
+
+}
