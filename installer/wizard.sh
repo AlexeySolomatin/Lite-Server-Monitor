b473cba3c21f4d7abaa5ae443443f889d9ae1a55
AlexeySolomatin <74787973+AlexeySolomatin@users.noreply.github.com>
2026-07-22 13:43:33 +0300

    Update wizard.sh

--- installer/wizard.sh
+++ installer/wizard.sh
@@ -7,13 +7,21 @@
 
 readonly LSM_SCREENS_DIR="${LSM_ROOT}/installer/screens"
 
+# shellcheck source=/dev/null
 source "${LSM_SCREENS_DIR}/common.sh"
+# shellcheck source=/dev/null
 source "${LSM_SCREENS_DIR}/welcome.sh"
+# shellcheck source=/dev/null
 source "${LSM_SCREENS_DIR}/install_mode.sh"
+# shellcheck source=/dev/null
 source "${LSM_SCREENS_DIR}/modules.sh"
+# shellcheck source=/dev/null
 source "${LSM_SCREENS_DIR}/notifications.sh"
+# shellcheck source=/dev/null
 source "${LSM_SCREENS_DIR}/smtp.sh"
+# shellcheck source=/dev/null
 source "${LSM_SCREENS_DIR}/ups.sh"
+# shellcheck source=/dev/null
 source "${LSM_SCREENS_DIR}/summary.sh"
 
 run_install_wizard() {
