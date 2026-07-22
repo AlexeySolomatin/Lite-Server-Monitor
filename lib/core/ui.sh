31beed6ae45505d2a33a4a9d7411e0ada6ca918b
AlexeySolomatin <74787973+AlexeySolomatin@users.noreply.github.com>
2026-07-22 15:21:20 +0300

    Update ui.sh

--- lib/core/ui.sh
+++ lib/core/ui.sh
@@ -2,120 +2,73 @@
 #
 # -----------------------------------------------------------------------------
 # Lite Server Monitor (LSM)
-# User Interface Library
+# Core UI & Terminal Formatting Helpers
 # -----------------------------------------------------------------------------
 
-[[ -n "${LSM_UI_LOADED:-}" ]] && return
-readonly LSM_UI_LOADED=1
+set -Eeuo pipefail
 
-#
-# Print application header
-#
+# Определение цветов (если терминал поддерживает ANSI)
+if [[ -t 1 ]]; then
+    COLOR_RESET="\033[0m"
+    COLOR_BOLD="\033[1m"
+    COLOR_RED="\033[0;31m"
+    COLOR_GREEN="\033[0;32m"
+    COLOR_YELLOW="\033[0;33m"
+    COLOR_BLUE="\033[0;34m"
+    COLOR_CYAN="\033[0;36m"
+else
+    COLOR_RESET=""
+    COLOR_BOLD=""
+    COLOR_RED=""
+    COLOR_GREEN=""
+    COLOR_YELLOW=""
+    COLOR_BLUE=""
+    COLOR_CYAN=""
+fi
+
+# Вывод баннера проекта (устраняет ошибку ui_banner)
+ui_banner() {
+    print_header
+}
+
 print_header() {
+    cat << EOF
 
-    echo
-    printf "%b%s%b\n" "${COLOR_CYAN}${COLOR_BOLD}" \
-        "Lite Server Monitor" \
-        "${COLOR_RESET}"
+${COLOR_CYAN}${COLOR_BOLD}=====================================================================${COLOR_RESET}
+${COLOR_CYAN}${COLOR_BOLD}   __    ____  __  __   (LSM) Lite Server Monitor                    ${COLOR_RESET}
+${COLOR_CYAN}${COLOR_BOLD}  / /   / __/ /  \/  |  Lightweight System Monitoring & Alerting     ${COLOR_RESET}
+${COLOR_CYAN}${COLOR_BOLD} / /___ \__ \ / /\_/ |  Version: ${PROJECT_VERSION:-1.0.0}                    ${COLOR_RESET}
+${COLOR_CYAN}${COLOR_BOLD}/_____//____//_/  /_/   Linux Server Management Tools                ${COLOR_RESET}
+${COLOR_CYAN}${COLOR_BOLD}=====================================================================${COLOR_RESET}
 
-    printf "%s\n" "Version ${PROJECT_VERSION}"
-    echo
-
+EOF
 }
 
-#
-# Print section title
-#
-print_section() {
-
-    echo
-    printf "%b== %s ==%b\n" \
-        "${COLOR_BLUE}" \
-        "$1" \
-        "${COLOR_RESET}"
-
+# Визуальное разделение блоков
+ui_section() {
+    local title="$1"
+    echo -e "\n${COLOR_BOLD}---> ${title}${COLOR_RESET}"
 }
 
-#
-# Wait for user
-#
-pause() {
-
-    read -rp "Press Enter to continue..."
-
+# Форматирование сообщений с иконками
+log_info() {
+    echo -e "[${COLOR_BLUE}INFO${COLOR_RESET}] $1"
 }
 
-#
-# Ask Yes / No
-#
-ask_yes_no() {
+log_success() {
+    echo -e "[${COLOR_GREEN} OK ${COLOR_RESET}] $1"
+}
 
-    local question="$1"
-    local default="${2:-Y}"
+log_warn() {
+    echo -e "[${COLOR_YELLOW}WARN${COLOR_RESET}] $1" >&2
+}
 
-    local prompt
+log_error() {
+    echo -e "[${COLOR_RED}FAIL${COLOR_RESET}] $1" >&2
+}
 
-    if [[ "${default^^}" == "Y" ]]; then
-        prompt="[Y/n]"
-    else
-        prompt="[y/N]"
+log_debug() {
+    if [[ "${LSM_DEBUG:-0}" == "1" ]]; then
+        echo -e "[${COLOR_CYAN}DEBUG${COLOR_RESET}] $1"
     fi
-
-    while true; do
-
-        read -rp "${question} ${prompt} " answer
-
-        answer="${answer:-$default}"
-
-        case "${answer,,}" in
-
-            y|yes)
-                return 0
-                ;;
-
-            n|no)
-                return 1
-                ;;
-
-            *)
-                log_warn "Please answer yes or no."
-                ;;
-
-        esac
-
-    done
-
-}
-
-#
-# Read text input
-#
-ask_input() {
-
-    local prompt="$1"
-    local default="${2:-}"
-
-    if [[ -n "${default}" ]]; then
-        read -rp "${prompt} [${default}]: " value
-        echo "${value:-$default}"
-    else
-        read -rp "${prompt}: " value
-        echo "${value}"
-    fi
-
-}
-
-#
-# Read password
-#
-ask_password() {
-
-    local prompt="$1"
-
-    read -rsp "${prompt}: " password
-
-    echo
-
-    echo "${password}"
-
 }
