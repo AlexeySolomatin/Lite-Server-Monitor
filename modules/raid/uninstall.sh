231027ed4355b5242f58a3c5133a3b20bd4aa103
AlexeySolomatin <74787973+AlexeySolomatin@users.noreply.github.com>
2026-07-22 13:57:33 +0300

    Update uninstall.sh

--- modules/raid/uninstall.sh
+++ modules/raid/uninstall.sh
@@ -1,1 +1,15 @@
+#!/usr/bin/env bash
+#
+# -----------------------------------------------------------------------------
+# Lite Server Monitor (LSM)
+# RAID Module Uninstaller
+# -----------------------------------------------------------------------------
 
+set -Eeuo pipefail
+
+deploy_remove_file /etc/systemd/system/lsm-raid.service
+deploy_remove_file /etc/systemd/system/lsm-raid.timer
+deploy_remove_file /etc/lsm/modules/raid.conf
+deploy_remove_directory /opt/lsm/modules/raid
+
+log_success "RAID module removed."
