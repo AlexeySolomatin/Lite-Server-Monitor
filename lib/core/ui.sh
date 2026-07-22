405b73610657e22c461eda3b79ceddf4d7a90f84
AlexeySolomatin <74787973+AlexeySolomatin@users.noreply.github.com>
2026-07-22 17:21:44 +0300

    Update ui.sh

--- lib/core/ui.sh
+++ lib/core/ui.sh
@@ -7,15 +7,15 @@
 
 set -Eeuo pipefail
 
-# Определение цветов (если терминал поддерживает ANSI)
+# Определение цветов (ANSI-C quoting гарантирует работу с 'cat' и 'echo')
 if [[ -t 1 ]]; then
-    COLOR_RESET="\033[0m"
-    COLOR_BOLD="\033[1m"
-    COLOR_RED="\033[0;31m"
-    COLOR_GREEN="\033[0;32m"
-    COLOR_YELLOW="\033[0;33m"
-    COLOR_BLUE="\033[0;34m"
-    COLOR_CYAN="\033[0;36m"
+    COLOR_RESET=$'\033[0m'
+    COLOR_BOLD=$'\033[1m'
+    COLOR_RED=$'\033[0;31m'
+    COLOR_GREEN=$'\033[0;32m'
+    COLOR_YELLOW=$'\033[0;33m'
+    COLOR_BLUE=$'\033[0;34m'
+    COLOR_CYAN=$'\033[0;36m'
 else
     COLOR_RESET=""
     COLOR_BOLD=""
@@ -26,7 +26,7 @@ else
     COLOR_CYAN=""
 fi
 
-# Вывод баннера проекта (устраняет ошибку ui_banner)
+# Вывод баннера проекта
 ui_banner() {
     print_header
 }
@@ -36,9 +36,9 @@ print_header() {
 
 ${COLOR_CYAN}${COLOR_BOLD}=====================================================================${COLOR_RESET}
 ${COLOR_CYAN}${COLOR_BOLD}   __    ____  __  __   (LSM) Lite Server Monitor                    ${COLOR_RESET}
-${COLOR_CYAN}${COLOR_BOLD}  / /   / __/ /  \/  |  Lightweight System Monitoring & Alerting     ${COLOR_RESET}
-${COLOR_CYAN}${COLOR_BOLD} / /___ \__ \ / /\_/ |  Version: ${PROJECT_VERSION:-1.0.0}                    ${COLOR_RESET}
-${COLOR_CYAN}${COLOR_BOLD}/_____//____//_/  /_/   Linux Server Management Tools                ${COLOR_RESET}
+${COLOR_CYAN}${COLOR_BOLD}  / /   / __/ /  \/  |   Lightweight System Monitoring & Alerting     ${COLOR_RESET}
+${COLOR_CYAN}${COLOR_BOLD} / /___ \__ \ / /\_/ |   Version: ${PROJECT_VERSION:-1.0.0}                    ${COLOR_RESET}
+${COLOR_CYAN}${COLOR_BOLD}/_____//____//_/   /_/   Linux Server Management Tools                ${COLOR_RESET}
 ${COLOR_CYAN}${COLOR_BOLD}=====================================================================${COLOR_RESET}
 
 EOF
@@ -50,25 +50,25 @@ ui_section() {
     echo -e "\n${COLOR_BOLD}---> ${title}${COLOR_RESET}"
 }
 
-# Форматирование сообщений с иконками
+# Форматирование сообщений с иконками (использование $* позволяет передавать фразы без кавычек)
 log_info() {
-    echo -e "[${COLOR_BLUE}INFO${COLOR_RESET}] $1"
+    echo "[${COLOR_BLUE}INFO${COLOR_RESET}] $*"
 }
 
 log_success() {
-    echo -e "[${COLOR_GREEN} OK ${COLOR_RESET}] $1"
+    echo "[${COLOR_GREEN} OK ${COLOR_RESET}] $*"
 }
 
 log_warn() {
-    echo -e "[${COLOR_YELLOW}WARN${COLOR_RESET}] $1" >&2
+    echo "[${COLOR_YELLOW}WARN${COLOR_RESET}] $*" >&2
 }
 
 log_error() {
-    echo -e "[${COLOR_RED}FAIL${COLOR_RESET}] $1" >&2
+    echo "[${COLOR_RED}FAIL${COLOR_RESET}] $*" >&2
 }
 
 log_debug() {
     if [[ "${LSM_DEBUG:-0}" == "1" ]]; then
-        echo -e "[${COLOR_CYAN}DEBUG${COLOR_RESET}] $1"
+        echo "[${COLOR_CYAN}DEBUG${COLOR_RESET}] $*"
     fi
-}
+fi
