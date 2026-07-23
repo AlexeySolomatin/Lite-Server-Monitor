9f2611559a553c19ddcb3c174f526a4a7390e8cb
AlexeySolomatin <74787973+AlexeySolomatin@users.noreply.github.com>
2026-07-23 22:53:05 +0300

    Update uninstall.sh

--- modules/disk/uninstall.sh
+++ modules/disk/uninstall.sh
@@ -7,6 +7,12 @@
 
 set -Eeuo pipefail
 
+# Останавливаем и отключаем службы
+if command -v systemctl >/dev/null 2>&1; then
+    systemctl disable --now lsm-disk.timer lsm-disk.service || true
+    systemctl daemon-reload || true
+fi
+
 deploy_remove_file /etc/systemd/system/lsm-disk.service
 deploy_remove_file /etc/systemd/system/lsm-disk.timer
 
