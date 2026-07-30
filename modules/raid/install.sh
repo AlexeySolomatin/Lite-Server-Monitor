d708125396faf32159185df20c9dfaefa6734261
AlexeySolomatin <74787973+AlexeySolomatin@users.noreply.github.com>
2026-07-30 11:09:45 +0300

    Update install.sh

--- modules/raid/install.sh
+++ modules/raid/install.sh
@@ -10,7 +10,7 @@ set -Eeuo pipefail
 MODULE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 LSM_ROOT="${LSM_ROOT:-/opt/lsm}"
 
-# Безопасный поиск и подгрузка библиотек ядра
+# Безопасный поиск и подгрузка библиотек ядра!
 if [[ -f "${LSM_ROOT}/lib/core/common.sh" ]]; then
     source "${LSM_ROOT}/lib/core/common.sh"
 elif [[ -f "${MODULE_DIR}/../../lib/core/common.sh" ]]; then
