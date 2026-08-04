35d25221a4af1038cae919284c3d37d9f2ee6b0d
AlexeySolomatin <74787973+AlexeySolomatin@users.noreply.github.com>
2026-08-04 13:42:40 +0300

    Update install_mode.sh

--- installer/screens/install_mode.sh
+++ installer/screens/install_mode.sh
@@ -1,43 +1,110 @@
 #!/usr/bin/env bash
-# shellcheck disable=SC2034
 #
 # -----------------------------------------------------------------------------
 # Lite Server Monitor (LSM)
-# Installation Mode Screen
+# Экран выбора режима установки
+# Путь: installer/screens/install_mode.sh
+#
+# Назначение:
+#   Отображает пользователю выбор режима установки:
+#
+#   standard - стандартная установка:
+#              устанавливаются все доступные модули,
+#              создаются все базовые конфигурации,
+#              пользователь настраивает только уведомления.
+#
+#   custom   - пользовательская установка:
+#              выбор модулей и дополнительных параметров.
 # -----------------------------------------------------------------------------
 
 set -Eeuo pipefail
 
-INSTALL_MODE="preset"
 
-screen_install_mode() {
+
+#
+# Режим установки по умолчанию
+#
+# standard - рекомендуемый вариант
+#
+
+INSTALL_MODE="standard"
+
+
+
+#
+# Экран выбора режима установки
+#
+
+screen_install_mode()
+{
+
     wizard_header
+
 
     echo -e "${CLR_BOLD}Выберите режим установки:${CLR_RESET}"
     echo
-    echo -e "  ${CLR_CYAN}1)${CLR_RESET} ${CLR_BOLD}Быстрая установка (Рекомендуется)${CLR_RESET}"
-    echo -e "     Установка стандартного комплекта модулей (disk, system, temperature, smart, login)."
+
+
+    echo -e "  ${CLR_CYAN}1)${CLR_RESET} ${CLR_BOLD}Стандартная установка (рекомендуется)${CLR_RESET}"
+    echo "     Установка всех доступных модулей LSM."
+    echo "     Создание стандартных конфигураций."
+    echo "     Настройка только Telegram и Email уведомлений."
     echo
-    echo -e "  ${CLR_CYAN}2)${CLR_RESET} ${CLR_BOLD}Пользовательская установка${CLR_RESET}"
-    echo -e "     Выбор конкретных модулей и тонкая настройка параметров."
+
+
+    echo -e "  ${CLR_CYAN}2)${CLR_RESET} ${CLR_BOLD}Настраиваемая установка${CLR_RESET}"
+    echo "     Выбор необходимых модулей."
+    echo "     Настройка уведомлений."
+    echo "     Дополнительная настройка UPS и ежедневных отчетов."
     echo
+
+
 
     while true; do
-        read -rp "$(echo -e "${CLR_BOLD}Выберите режим [1-2]${CLR_RESET} [${CLR_YELLOW}1${CLR_RESET}]: ")" answer
+
+
+        read -rp \
+            "$(echo -e "${CLR_BOLD}Выберите режим [1-2]${CLR_RESET} [${CLR_YELLOW}1${CLR_RESET}]: ")" \
+            answer
+
+
         answer="${answer:-1}"
 
+
+
         case "${answer}" in
+
+
             1)
-                INSTALL_MODE="preset"
+
+                INSTALL_MODE="standard"
+
                 break
+
                 ;;
+
+
             2)
+
                 INSTALL_MODE="custom"
+
                 break
+
                 ;;
+
+
             *)
-                echo -e "${CLR_RED}Неверный выбор. Пожалуйста, введите 1 или 2.${CLR_RESET}"
+
+                echo -e \
+                    "${CLR_RED}Неверный выбор. Введите 1 или 2.${CLR_RESET}"
+
                 ;;
+
+
         esac
+
+
     done
+
+
 }
