86e2717c856531d1b1ea8641e07a1c9d2686733b
AlexeySolomatin <74787973+AlexeySolomatin@users.noreply.github.com>
2026-08-04 14:36:38 +0300

    Update ups.sh

--- installer/screens/ups.sh
+++ installer/screens/ups.sh
@@ -3,53 +3,186 @@
 #
 # -----------------------------------------------------------------------------
 # Lite Server Monitor (LSM)
-# UPS Configuration Screen
+# Экран настройки мониторинга ИБП
+# Путь: installer/screens/ups.sh
+#
+# Назначение:
+#   Управляет включением мониторинга ИБП через apcupsd.
+#
+#   Стандартная установка:
+#       мониторинг ИБП включается автоматически.
+#
+#   Настраиваемая установка:
+#       пользователь выбирает включать или нет.
 # -----------------------------------------------------------------------------
 
 set -Eeuo pipefail
 
+
+
+#
+# Параметры ИБП по умолчанию
+#
+
 INSTALL_UPS=false
+
 UPS_PROFILE=""
 
-screen_ups() {
+
+
+#
+# Экран настройки ИБП
+#
+
+screen_ups()
+{
+
+
     wizard_header
 
-    echo -e "${CLR_BOLD}Настройка мониторинга ИБП (APC UPS):${CLR_RESET}"
-    echo "Позволяет отслеживать уровень заряда, напряжение и статус питания от батареи."
+
+
+    #
+    # Стандартная установка
+    #
+
+    if [[ "${INSTALL_MODE:-standard}" == "standard" ]]; then
+
+
+        INSTALL_UPS=true
+
+        UPS_PROFILE="default"
+
+
+
+        echo -e \
+            "${CLR_BOLD}Мониторинг ИБП:${CLR_RESET}"
+
+
+        echo
+
+        echo -e \
+            "${CLR_GREEN}✓ Мониторинг ИБП включен автоматически.${CLR_RESET}"
+
+
+        echo -e \
+            "Профиль: ${CLR_CYAN}${UPS_PROFILE}${CLR_RESET}"
+
+
+        return 0
+
+
+    fi
+
+
+
+    #
+    # Настраиваемая установка
+    #
+
+    echo -e \
+        "${CLR_BOLD}Настройка мониторинга ИБП (APC UPS):${CLR_RESET}"
+
+
+    echo \
+        "Отслеживание заряда батареи, питания и состояния ИБП."
+
+
     echo
 
-    if ! wizard_yes_no "Включить модуль мониторинга ИБП (APC UPS)?" "n"; then
+
+
+    if ! wizard_yes_no \
+        "Включить мониторинг ИБП?" \
+        "n"; then
+
+
         INSTALL_UPS=false
+
         UPS_PROFILE=""
+
+
         return 0
+
+
     fi
+
+
 
     INSTALL_UPS=true
 
+
+
     echo
-    echo -e "  ${CLR_CYAN}1)${CLR_RESET} APC Стандартный профиль (apcupsd / localhost:3551)"
-    echo -e "  ${CLR_CYAN}2)${CLR_RESET} Настроить параметры позже"
+
+    echo -e \
+        "  ${CLR_CYAN}1)${CLR_RESET} APC стандартный профиль (apcupsd)"
+
+
+    echo -e \
+        "  ${CLR_CYAN}2)${CLR_RESET} Настроить параметры позже"
+
+
     echo
+
+
 
     while true; do
-        read -rp "$(echo -e "${CLR_BOLD}Выберите профиль [1-2]${CLR_RESET} [${CLR_YELLOW}1${CLR_RESET}]: ")" answer
+
+
+        read -rp \
+            "$(echo -e "${CLR_BOLD}Выберите профиль [1-2]${CLR_RESET} [${CLR_YELLOW}1${CLR_RESET}]: ")" \
+            answer
+
+
+
         answer="${answer:-1}"
 
+
+
         case "${answer}" in
+
+
             1)
+
+
                 UPS_PROFILE="default"
+
                 break
+
                 ;;
+
+
             2)
+
+
                 UPS_PROFILE="later"
+
                 break
+
                 ;;
+
+
             *)
-                echo -e "${CLR_RED}Неверный выбор. Пожалуйста, введите 1 или 2.${CLR_RESET}"
+
+
+                echo -e \
+                    "${CLR_RED}Введите 1 или 2.${CLR_RESET}"
+
                 ;;
+
+
         esac
+
+
     done
 
+
+
     echo
-    echo -e "${CLR_GREEN}✓ Поддержка ИБП включена (${UPS_PROFILE}).${CLR_RESET}"
+
+    echo -e \
+        "${CLR_GREEN}✓ Мониторинг ИБП включен (${UPS_PROFILE}).${CLR_RESET}"
+
+
 }
